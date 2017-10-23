124746eb8d1fec3cb340e6a24c1250625922f0d7
Xiaoda Zhang <xiaoda-zh@atlassian.com>
2017-10-23 17:40:23 +0800

    first push

---
+++ src/mapreduce/schedule.go
@@ -0,0 +1,26 @@
+package mapreduce
+
+// schedule starts and waits for all tasks in the given phase (Map or Reduce).
+func (mr *Master) schedule(phase jobPhase) {
+	var ntasks int
+	var nios int // number of inputs (for reduce) or outputs (for map)
+	switch phase {
+	case mapPhase:
+		ntasks = len(mr.files)
+		nios = mr.nReduce
+	case reducePhase:
+		ntasks = mr.nReduce
+		nios = len(mr.files)
+	}
+
+	debug("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
+
+	// All ntasks tasks have to be scheduled on workers, and only once all of
+	// them have been completed successfully should the function return.
+	// Remember that workers may fail, and that any given worker may finish
+	// multiple tasks.
+	//
+	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+	//
+	debug("Schedule: %v phase done\n", phase)
+}
